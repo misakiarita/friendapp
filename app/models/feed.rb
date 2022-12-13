c4cb6d6aae66ba6d5e22d3a57859d1e51d1d815d
misaki arita <misaki.arita0838@gmail.com>
2022-12-13 17:22:59 +0900

    フロント部分(ulタグ)の変更

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,6 @@
 class Feed < ApplicationRecord
     mount_uploader :image, ImageUploader
-    validates :content, 
+    validates :content, :title,
     length: { minimum: 1}
     belongs_to :user
 end
